82f096371aa7bfa2b69bc96bc66c95b722acb80f
Surat Pyari <suratpyari.db21@gmail.com>
2011-05-02 16:01:52 +0530

    seed modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,15 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
-# Refinery seeds
-# Dir[Rails.root.join('db', 'seeds', '*.rb').to_s].each do |file|
-#   puts "Loading db/seeds/#{file.split(File::SEPARATOR).last}"
-#   load(file)
-# end
+Refinery seeds
+Dir[Rails.root.join('db', 'seeds', '*.rb').to_s].each do |file|
+  puts "Loading db/seeds/#{file.split(File::SEPARATOR).last}"
+  load(file)
+end
 require 'csv'
-# CSV.foreach(File.open('db/seeds/members.csv')) do |row|
-#   Member.create(:facebook_path => row[0], :name => row[1], :description => row[2]) unless row[1]=='name'
-# end
+CSV.foreach(File.open('db/seeds/members.csv')) do |row|
+  Member.create(:facebook_path => row[0], :name => row[1], :description => row[2]) unless row[1]=='name'
+end
 YAML.load_file("#{Rails.root}/db/seeds/portfolio.yml").each  do |k, v|
   portfolio = Portfolio.create!(:url => v["url"], :page => Page.find_by_title(v["page_title"])[0])
   portfolio.translations << PortfolioTranslation.new(:locale => 'en', :name => v["en_name"], :description => v["en_description"])
